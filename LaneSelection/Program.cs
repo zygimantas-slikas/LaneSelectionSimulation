11b8997ba14e014663c72af48b894443aa3299e0
Žygimantas Šlikas <zygimantas.slikas1999@gmail.com>
2021-12-22 20:14:50 +0200

    Add project files.

---
+++ LaneSelection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaneSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
